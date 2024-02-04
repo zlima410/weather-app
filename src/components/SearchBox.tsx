0deb896bac44019e2d78bd88497389bbb1812797
zlima410 <endercraft68@gmail.com>
2024-02-04 18:10:27 -0500

    Refactor SearchBox component to include props

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -3,13 +3,32 @@
 import React from "react";
 import { IoSearchSharp } from "react-icons/io5";
 
-type Props = {};
+type Props = {
+    className?: string
+    value: string
+    onChange: React.ChangeEventHandler<HTMLInputElement> | undefined
+    onSubmit: React.FormEventHandler<HTMLFormElement> | undefined
+};
 
-export default function SearchBox({}: Props) {
+export default function SearchBox(props: Props) {
   return (
-    <form className="flex relative items-center justify-center h-10">
-      <input type="text" />
-      <button>
+    <form 
+        onSubmit={props.onSubmit} 
+        className="flex relative items-center 
+        justify-center h-10"
+    >
+      <input
+        type="text"
+        value={props.value}
+        onChange={props.onChange}
+        placeholder="Search location..."
+        className="px-4 py-2 w-[230px] border border-gray-300 rounded-l-md focus:outline-none
+         focus:border-blue-500 h-full"
+      />
+      <button
+        className="px-4 py-[9px] bg-blue-500
+      text-white rounded-r-md focus:outline-none 
+      hover:bg-blue-600 h-full">
         <IoSearchSharp />
       </button>
     </form>
