66e0dee87425e33f981ad77fd7823ed72d64cdff
zlima410 <endercraft68@gmail.com>
2024-02-04 16:11:04 -0500

    Add react-icons package and display sun icon in Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 /** @format */
 
 import React from "react";
+import { FaSun } from "react-icons/fa";
 
 type Props = {};
 
@@ -10,6 +11,7 @@ export default function Navabr({}: Props) {
       <div className="h-[80px] w-full flex justify-between items-center max-w-7xl px-3 mx-auto">
         <p className="flex items-center justify-center gap-2">
         <h2 className="text-gray-500 text-3xl">Weather</h2>
+            <FaSun />
         </p>
       </div>
     </nav>
