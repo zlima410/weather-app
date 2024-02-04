56e59588c12d2109bb86f1749fe158f1e42b4ad0
zlima410 <endercraft68@gmail.com>
2024-02-04 18:43:47 -0500

    Begin constructing API call to weather API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
 /** @format */
 
-import dynamic from 'next/dynamic'
+import dynamic from "next/dynamic";
 import Image from "next/image";
 
-const NoSSR = dynamic(() => import('@/components/Navbar'), { ssr: false })
+const NoSSR = dynamic(() => import("@/components/Navbar"), { ssr: false });
 
+// https://api.openweathermap.org/data/2.5/forecast?q=pune&appid=3a6361e0b290e5b3a9cdd6928941a73c&cnt=56
 export default function Home() {
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
-        <NoSSR />
+      <NoSSR />
     </div>
   );
 }
