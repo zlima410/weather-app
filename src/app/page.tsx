2f192ce10b049605fde999accd8e3f49a6f26f78
zlima410 <endercraft68@gmail.com>
2024-02-04 19:07:41 -0500

    Add WeatherData and WeatherDetail interfaces

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,61 @@ import Image from "next/image";
 
 const NoSSR = dynamic(() => import("@/components/Navbar"), { ssr: false });
 
+interface WeatherData {
+  cod: string;
+  message: number;
+  cnt: number;
+  list: WeatherDetail[];
+  city: {
+    id: number;
+    name: string;
+    coord: {
+      lat: number;
+      lon: number;
+    };
+    country: string;
+    population: number;
+    timezone: number;
+    sunrise: number;
+    sunset: number;
+  };
+}
+
+interface WeatherDetail {
+  dt: number;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    sea_level: number;
+    grnd_level: number;
+    humidity: number;
+    temp_kf: number;
+  };
+  weather: {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }[];
+  clouds: {
+    all: number;
+  };
+  wind: {
+    speed: number;
+    deg: number;
+    gust: number;
+  };
+  visibility: number;
+  pop: number;
+  sys: {
+    pod: string;
+  };
+  dt_txt: string;
+}
+
 // https://api.openweathermap.org/data/2.5/forecast?q=pune&appid=3a6361e0b290e5b3a9cdd6928941a73c&cnt=56
 export default function Home() {
   return (
