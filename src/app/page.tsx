da72c090d9abe4553956cc5a5b1336a8d1b5b72c
zlima410 <endercraft68@gmail.com>
2024-02-04 20:20:52 -0500

    Refactor loading animation and add skeleton structure for data display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -72,16 +72,29 @@ export default function Home() {
     return data;
   });
 
-  if (isLoading) 
-  return (
-    <div className="flex items-center max-h-screen justify-center">
+  if (isLoading)
+    return (
+      <div className="flex items-center max-h-screen justify-center">
         <p className="animate-bounce">Loading...</p>
-    </div>
-);
+      </div>
+    );
 
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
       <Navbar />
+      <main className="px-3 max-w-7xl mx-auto flex flex-col gap-9 w-full pb-10 pt-4">
+        {/* Today's data */}
+        <section>
+            <div>
+                <h2 className="flex gap-1 text-2xl items-end">
+                    <p></p>
+                </h2>
+                <div></div>
+            </div>
+        </section>
+        {/* 7 day forecast data*/}
+        <section></section>
+      </main>
     </div>
   );
 }
