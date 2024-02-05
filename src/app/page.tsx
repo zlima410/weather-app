9dea8e155c6a82a5c83b7948f028cb2a349a0542
zlima410 <endercraft68@gmail.com>
2024-02-04 20:15:46 -0500

    Refactor loading message in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -72,9 +72,12 @@ export default function Home() {
     return data;
   });
 
-  console.log("data");
-
-  if (isLoading) return "Loading...";
+  if (isLoading) 
+  return (
+    <div className="flex items-center max-h-screen justify-center">
+        <p className="animate-bounce">Loading...</p>
+    </div>
+);
 
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
